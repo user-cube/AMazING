7a10937358c3a697a0d531289ec6fb4b11b9c2dd
Rui Coelho <ruicoelho@ua.pt>
2020-03-13 21:38:43 +0000

    basic login

--- Frontend/app/views.py
+++ Frontend/app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from datetime import datetime
+
+def home(request):
+    tparams = {
+        'title': 'Home Page',
+        'year': datetime.now().year,
+    }
+    return render(request, 'index.html', tparams)
