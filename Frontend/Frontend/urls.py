4af3b7bae500602c1d8eb0e36a1e8be6d74fccef
Rui Coelho <ruicoelho@ua.pt>
2020-03-13 22:19:31 +0000

    login css

--- Frontend/Frontend/urls.py
+++ Frontend/Frontend/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from app import views
+from datetime import datetime
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- Frontend/app/views.py
+++ Frontend/app/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from datetime import datetime
 
 def home(request):
-    tparams = {
-        'title': 'Home Page',
-        'year': datetime.now().year,
-    }
-    return render(request, 'index.html', tparams)
+    if request.user.is_authenticated:
+        tparams = {
+            'title': 'Home Page',
+            'year': datetime.now().year,
+        }
+        return render(request, 'index.html', tparams)
+    else:
+        return redirect('login')
