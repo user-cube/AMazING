305eb432ccc7381c591e31a296ba8e760d9c8af5
Rui Coelho <ruicoelho@ua.pt>
2020-04-22 18:57:40 +0100

    tokenizer

--- Frontend/app/views.py
+++ Frontend/app/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render, redirect
-
-# Create your views here.
+from django.http import HttpResponse, HttpResponseNotFound
+from datetime import datetime, timedelta, timezone
+import jwt
+# Create your views here
+# 192.168.85.209
 from datetime import datetime
+import requests
+from datetime import datetime, timedelta, timezone
+
+from jwt import (
+    JWT,
+    jwk_from_dict,
+    jwk_from_pem,
+)
+from jwt.utils import get_int_from_datetime
+
+
+jwtManager = JWT()
+with open("app/certificates/privateKey.pem", 'rb') as reader:
+    input_key = jwt.jwk_from_pem(reader.read())
 
 def home(request):
     if request.user.is_authenticated:
@@ -10,5 +27,57 @@ def home(request):
             'year': datetime.now().year,
         }
         return render(request, 'index.html', tparams)
+    else:
+        return redirect('login')
+
+def profile(request):
+    """
+    User profile view.
+    Args:
+        request: request data;
+
+    Returns:
+        When status code is 200, display
+        user profile page, otherwise
+        404 page.
+
+    """
+    if request.user.is_authenticated:
+
+        message = {
+            'email': request.user.email,
+            'iat': get_int_from_datetime(datetime.now(timezone.utc)),
+            'exp': get_int_from_datetime(
+                datetime.now(timezone.utc) + timedelta(minutes=1)),
+        }
+
+        token = jwtManager.encode(message, input_key, alg='RS256')
+        r = requests.get("http://127.0.0.1:5000/profile", headers = {'Authorization': token})
+
+        if r.status_code != 200:
+            return HttpResponseNotFound()
+
+        json = r.json()
+        """
+        Example of expected reponse:
+            {
+                email: "ruicoelho@ua.pt",
+                id: 200,
+                name: "Rui",
+                numtests: 130,
+                picture: null,
+                registerdate: "03-05-2019",
+                role: 1
+            }
+        """
+        tparams = {
+            'name' : json['name'],
+            'email': json['email'],
+            'numtests' : json['numtests'],
+            'registerdate': json['registerdate'],
+            'role': json['role']
+        }
+        print(tparams)
+
     else:
         return redirect('login')
