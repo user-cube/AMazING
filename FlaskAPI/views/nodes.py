73d452979e0224afd008210479ff7c41a6162ba1
Jean Brito <jean18.jh@gmail.com>
2020-05-31 10:45:32 +0100

    Fixed DELETE POSTs

--- FlaskAPI/views/nodes.py
+++ FlaskAPI/views/nodes.py
@@ -111,7 +111,7 @@ def delete_node(id):
         results.status_code = status.HTTP_404_NOT_FOUND
         return results
     apu.delete(apu)
-    return jsonify(apu.serializable)
+    return jsonify()
 
 
 # PROXY Settings
