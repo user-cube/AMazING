0ff7d319b964c8762db437cca21a1c4ec680cb50
Rui Coelho <ruicoelho@ua.pt>
2020-06-01 03:36:42 +0100

    timeout

--- FlaskAPI/views/nodes.py
+++ FlaskAPI/views/nodes.py
@@ -178,7 +178,7 @@ def send_node_command__to_interface_as_post(id, interface, command):
         try:
             ssid = raw_data['SSID']
             password = raw_data['password']
-            response = requests.post(url=apu_request, json={'SSID': ssid, 'PASS' : password}, timeout=2)
+            response = requests.post(url=apu_request, json={'SSID': ssid, 'PASS' : password}, timeout=5)
             results = jsonify(response.json())
             results.status_code = response.status_code
             return results
@@ -189,7 +189,7 @@ def send_node_command__to_interface_as_post(id, interface, command):
     else:   
         try:
             ip = raw_data['ip']
-            response = requests.post(url=apu_request, json={'ip': ip}, timeout=2)
+            response = requests.post(url=apu_request, json={'ip': ip}, timeout=5)
             results = jsonify(response.json())
             results.status_code = response.status_code
             return results
